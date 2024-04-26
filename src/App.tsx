82fdfed5bfbc10a1c77f80ee6b1112be83a055ea
rahathasanDev <devrahat.hasan@gmail.com>
2024-04-26 18:10:21 +0600

    Initialize Store in redux app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 function App() {
-
   return (
     <>
-      <h1>React with Redux</h1>
+      <div className="h-screen w-full flex  justify-center items-center">
+        <div className="flex">
+          <button className="px-3 py-3 rounded-md font-semibold text-white bg-green-500 text-xl">Increment</button>
+          <h1 className="text-3xl mx-5">0</h1>
+          <button className="px-3 py-3 rounded-md font-semibold text-white bg-red-500 text-xl">Decrement</button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
